b0b33d0105f091febc02ef1bffb06c109e9355c5
John Evans <lgastako@gmail.com>
2014-08-14 01:39:34 -0700

    Better.

--- js/ramdos.js
+++ js/ramdos.js
@@ -4,21 +4,17 @@ angular.module("RamdosApp", []) //, ["ngResource"]);
 
 .controller("NavController", function($scope) {
     $scope.navItems = [
-        {"label": "Graph",
-         "href": "#graph"},
-        {"label": "Search",
-         "href": "#search"},
-    ];
+        { label: "Graph",  href: "#graph" },
+        { label: "Search", href: "#search" },
+    ]
 })
 
 .controller("SearchController", function($scope) {
-    $scope.query = null;
+    $scope.query = null
 })
 
-.controller("GraphController", function($scope) {
-
-})
+.controller("GraphController", function($scope) {})
 
 .controller("NullStateController", function($scope) {})
 
-;
+
